76ac1a0ea92ed85cbfd7520e6eb8cbc09665af53
Olpeach06 <lavrenteva.olesiii.06@mail.ru>
2025-03-17 10:59:37 +0300

    zdkfgjkadfjg

--- KinoCatalog/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KinoCatalog.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
